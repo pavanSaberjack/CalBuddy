6561ded8e8c04dd2bcb619fe18b6274937611002
Pavan Itagi <pavan.itagi@ymedialabs.com>
2023-01-12 12:49:57 +0530

    Added 404 page

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// routes
 const addEventsRouter = require('./routes/add-event');
 const eventListRouter = require('./routes/event-list');
 const rootDir = require('./util/path');
+
+//controllers
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -19,5 +23,7 @@ app.use(express.static(path.join(rootDir, 'public')));
 
 app.use(addEventsRouter);
 app.use(eventListRouter);
+
+app.use(errorController.get404);
 
 app.listen(3000);
